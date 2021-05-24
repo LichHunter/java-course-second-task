7699af8cfc60a8ccd821361776c36123509d2c8c
LichHunter <alexander0derevianko@gmail.com>
2021-05-24 16:57:00 +0100

    In the beginning was the Word

---
+++ src/main/java/com/second/task/App.java
@@ -0,0 +1,14 @@
+package com.second.task;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ */
+public class App {
+	private static Logger logger = Logger.getLogger(App.class);
+
+	public static void main(String[] args) {
+		logger.info("Starting application");
+	}
+}
